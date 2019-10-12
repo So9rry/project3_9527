464d663668d6330ac6ff8e20cf632307e043c8a3
wangdiskater <996057150@qq.com>
2019-10-12 19:56:45 +0800

    项目搭建

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/persistence/dao/UserMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.persistence.dao;
+
+import com.stylefeng.guns.rest.persistence.model.User;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author wangdi
+ * @since 2019-10-12
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
